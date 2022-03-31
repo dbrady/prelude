72ffc5384fb03cf4f338640ee44f4f44760dfae8
David Brady <dbrady@shinybit.com>
2022-03-31 09:46:38 -0600

    I already had that line

--- personal/preload/melpa.el
+++ personal/preload/melpa.el
@@ -7,4 +7,4 @@
 (when (< emacs-major-version 24)
   ;; For important compatibility libraries like cl-lib
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
-(package-initialize) ;; You might already have this line
+;;(package-initialize) ;; You might already have this line
