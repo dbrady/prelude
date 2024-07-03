ff1b2af66c2518337f033489de8d43b36941146e
David Brady <dbrady@shinybit.com>
2024-07-02 21:13:13 -0600

    Add Dave Solarized 2022-theme.el

---
+++ Dave Solarized 2022-theme.el
@@ -0,0 +1,10 @@
+(deftheme Dave Solarized 2022
+  "Created 2022-09-01.")
+
+(custom-theme-set-variables
+ 'Dave Solarized 2022
+ '(custom-safe-themes '("645f4d91954f7416a6a3df1c9049e60a3e0811c343f809f300898c5ff90821c2" "fc48cc3bb3c90f7761adf65858921ba3aedba1b223755b5924398c666e78af8b" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default))
+ '(flycheck-checker-error-threshold 999)
+ '(package-selected-packages '(csv-mode yaml-mode web-mode lsp-ui lsp-mode json-mode js2-mode rainbow-mode elisp-slime-nav rainbow-delimiters company counsel swiper ivy exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree super-save smartrep smartparens operate-on-number nlinum move-text magit projectile imenu-anywhere hl-todo guru-mode git-modes git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major crux browse-kill-ring anzu ag ace-window)))
+
+(provide-theme 'Dave Solarized 2022)
